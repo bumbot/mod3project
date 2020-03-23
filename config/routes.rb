6e5729a13cba1c3835a2577c53c8e4b33d6a6e36
bumbot <pkim1295@gmail.com>
2020-03-23 15:06:55 -0400

    added minor line of code in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   resources :battles, only: [:index, :show]
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # action cable
+  mount ActionCable.server => '/cable'
 end
